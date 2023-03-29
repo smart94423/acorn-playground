0978915edd5e833e69e2f061f59cd4e5aa27bf98
smart94423 <smart94423@gmail.com>
2023-03-29 21:47:59 +0200

    wip

--- parseWithAcorn.ts
+++ parseWithAcorn.ts
@@ -16,41 +16,40 @@ function replaceImportStatements(code: string): string {
 
   const spliceOperations: SpliceOperation[] = [];
 
-  body
-    .filter((node) => node.type === "ImportDeclaration")
-    .forEach((node) => {
-      assert(node.type === "ImportDeclaration");
-      const file = node.source.value;
-      assert(file);
+  body.forEach((node) => {
+    if (node.type !== "ImportDeclaration") return;
 
-      let replacement = "";
-      node.specifiers.forEach((specifier) => {
-        assert(
-          specifier.type === "ImportSpecifier" ||
-            specifier.type === "ImportDefaultSpecifier"
-        );
-        const importVar = specifier.local.name;
-        const importName = (() => {
-          if (specifier.type === "ImportDefaultSpecifier") return "default";
-          {
-            const imported = (specifier as any).imported as
-              | Identifier
-              | undefined;
-            if (imported) return imported.name;
-          }
-          return importVar;
-        })();
-        replacement += `const ${importVar} = '__import|${file}|${importName}';`;
-      });
-      assert(replacement.length > 0);
+    const file = node.source.value;
+    assert(file);
 
-      const { start, end } = node;
-      spliceOperations.push({
-        start,
-        end,
-        replacement,
-      });
+    let replacement = "";
+    node.specifiers.forEach((specifier) => {
+      assert(
+        specifier.type === "ImportSpecifier" ||
+          specifier.type === "ImportDefaultSpecifier"
+      );
+      const importVar = specifier.local.name;
+      const importName = (() => {
+        if (specifier.type === "ImportDefaultSpecifier") return "default";
+        {
+          const imported = (specifier as any).imported as
+            | Identifier
+            | undefined;
+          if (imported) return imported.name;
+        }
+        return importVar;
+      })();
+      replacement += `const ${importVar} = '__import|${file}|${importName}';`;
     });
+    assert(replacement.length > 0);
+
+    const { start, end } = node;
+    spliceOperations.push({
+      start,
+      end,
+      replacement,
+    });
+  });
 
   const codeMod = spliceMany(code, spliceOperations);
   return codeMod;
